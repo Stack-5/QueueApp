db7e135c2bf112a9a8bd2e0e095f0c029b3a06b1
Richmond Baltazar <baltazarrichmond31@gmail.com>
2025-02-16 17:06:28 +0800

    Add endpoint to retrieve current queue ID

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,15 +2,29 @@ import * as v2 from "firebase-functions/v2";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import queueRoutes from "./routers/queueRoutes";
+import cors from "cors";
 
 dotenv.config();
 const app: Express = express();
 
+const allowedOrigins = ["http://localhost:3000", process.env.NEUQUEUE_ROOT_URL];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+app.use(express.json());
+
 app.get("/", (req:Request, res:Response) => {
   res.send("<h1>Welcome to NEUQUEUE</h1>");
 });
-
-app.use(express.json());
 
 app.use("/queue", queueRoutes);
 
--- backend/functions/src/routers/queueRoutes.ts
+++ backend/functions/src/routers/queueRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/verifyJWT";
-import { addQueue, generateQrCode } from "../controllers/queueControllers";
+import { addQueue, generateQrCode, getCurrentQueueID } from "../controllers/queueControllers";
 
 
 // eslint-disable-next-line new-cap
@@ -8,6 +8,6 @@ const router: Router = Router();
 
 router.get("/qrcode", generateQrCode);
 router.post("/add", verifyJWT, addQueue);
-
+router.get("/current", verifyJWT, getCurrentQueueID);
 
 export default router;
