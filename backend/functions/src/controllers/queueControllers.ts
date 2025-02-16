b184ff055afd4460c36c3a28eda072d957a1fb3a
Richmond Baltazar <baltazarrichmond31@gmail.com>
2025-02-16 14:48:55 +0800

    used env

--- backend/functions/src/controllers/queueControllers.ts
+++ backend/functions/src/controllers/queueControllers.ts
@@ -6,17 +6,18 @@ import { addToQueueSchema } from "../zod-schemas/addToQueue";
 import QueueRequest from "../types/QueueRequest";
 
 const SECRET_KEY = process.env.JWT_SECRET;
+const NEUQUEUE_ROOT_URL = process.env.NEUQUEUE_ROOT_URL;
 
 export const generateQrCode = async (req: Request, res: Response) => {
   try {
     const payload = {
       id: Math.random().toString(36).substring(2, 10),
     };
-    if (!SECRET_KEY) {
+    if (!SECRET_KEY || !NEUQUEUE_ROOT_URL) {
       throw new Error("Missing Secret in environmental variables!");
     }
     const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "3m"});
-    const url = `https://retchizu-94b36.web.app?token=${token}`;
+    const url = `${NEUQUEUE_ROOT_URL}?token=${token}`;
     const qrCodeDataUrl = await QRcode.toDataURL(url);
 
     res.status(201).json({qrCode: qrCodeDataUrl, token: token});
