e93682a3ddca2a0ebe31bf8d720b77c2708f253f
Erlyn De Leon <erlynqueendeleon81@gmail.com>
2025-02-26 12:47:17 +0800

    Revert "PendingAccount Screen UI"
    This reverts commit ff4514c0122420fa6797efee3960f7f1aa8224cf.

--- frontend/mobile/app/(main)/default/pending.tsx
+++ frontend/mobile/app/(main)/default/pending.tsx
@@ -1,72 +1,20 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { auth } from "../../../firebaseConfig";
-import { useSignOutAuthStateListener } from "../../../hooks/useSignOutAuthStateListener";
-import { useUserContext } from "../../../contexts/UserContext";
-import NeuQueueButtonYellow from "../../../components/NeuQueueButtonYellow";
-import NeuQueueLogo from "../../../components/NeuQueueLogo";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { auth } from '../../../firebaseConfig'
+import { useSignOutAuthStateListener } from '../../../hooks/useSignOutAuthStateListener'
+import { useUserContext } from '../../../contexts/UserContext'
+import NeuQueueButtonYellow from '../../../components/NeuQueueButtonYellow'
 
 const PendingScreen = () => {
-  const { userInfo } = useUserContext();
-  console.log(userInfo?.role);
+  const {userInfo} = useUserContext();
+  console.log(userInfo?.role)
   useSignOutAuthStateListener();
-
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#F9FAFB",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <NeuQueueLogo />
-      <Text
-        style={{
-          fontFamily: "lexendmedium",
-          fontSize: hp(2.4),
-          color: "#333333",
-          marginTop: hp(2),
-        }}
-      >
-        Waiting for Admin Approval
-      </Text>
-      <View
-        style={{
-          backgroundColor: "#0077B6",
-          width: "80%",
-          padding: wp(4),
-          marginTop: hp(2),
-          borderRadius: wp(3),
-          borderColor: "#FFBF00",
-          borderWidth: wp(0.4),
-        }}
-      >
-        <Text
-          style={{
-            color: "#F9FAFB",
-            fontFamily: "lexendregular",
-            fontSize: hp(2),
-            textAlign: "center",
-            letterSpacing: hp(0.2),
-          }}
-        >
-          Your account has been created, but you don’t have a role assigned yet.
-          Please wait while the admin reviews your request.
-        </Text>
-      </View>
-      <View style={{ width: "80%", marginTop: hp(1) }}>
-        <NeuQueueButtonYellow
-          title="Sign Out"
-          buttonFn={async () => await auth.signOut()}
-        />
-      </View>
+    <View>
+      <Text>Admin is reviewing your account</Text>
+      <NeuQueueButtonYellow title="Sign Out" buttonFn={async() => await auth.signOut()}/>
     </View>
-  );
-};
+  )
+}
 
-export default PendingScreen;
+export default PendingScreen
