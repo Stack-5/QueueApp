3d3747f887a409322f2bb8296494aa54449139eb
Richmond Baltazar <baltazarrichmond31@gmail.com>
2025-03-15 11:26:46 +0800

    verification of customerToken
    phone number validation

--- backend/functions/src/routers/queueRoutes.ts
+++ backend/functions/src/routers/queueRoutes.ts
@@ -6,6 +6,7 @@ import {
   getAvailableStation,
   getLatestQueueIDs,
   getQueuePosition,
+  verifyCustomerToken,
 } from "../controllers/queueControllers";
 
 // eslint-disable-next-line new-cap
@@ -13,8 +14,9 @@ const router: Router = Router();
 
 router.get("/qrcode", generateQrCode);
 router.post("/add", verifyValidQueueJWT, verifyUsedToken, addQueue);
-router.get("/available-stations", verifyValidQueueJWT, verifyUsedToken, getAvailableStation);
+router.post("/available-stations", verifyValidQueueJWT, verifyUsedToken, getAvailableStation);
 router.post("/queue-position", verifyValidQueueJWT, getQueuePosition);
 router.get("/get-latest", verifyValidQueueJWT, getLatestQueueIDs);
+router.get("/verify-on-mount", verifyValidQueueJWT, verifyCustomerToken);
 
 export default router;
--- backend/functions/src/zod-schemas/addToQueue.ts
+++ backend/functions/src/zod-schemas/addToQueue.ts
@@ -1,8 +1,13 @@
-import { number, object, string, enum as zEnum} from "zod";
+import { number, object, string, enum as zEnum } from "zod";
 
 export const addToQueueSchema = object({
-  purpose: zEnum(["payment", "auditing", "clinic", "registrar"]).default("payment"),
-  cellphoneNumber: string(),
+  purpose: zEnum(["payment", "auditing", "clinic", "registrar"]).default(
+    "payment"
+  ),
+  cellphoneNumber: string().regex(
+    /^\+63\d{10}$/,
+    "Invalid phone number format. Use +63XXXXXXXXXX"
+  ),
   timestamp: number().default(() => Date.now()),
   customerStatus: zEnum(["pending", "complete"]).default("pending"),
   stationID: string(),
